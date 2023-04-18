19b6751b076c3244337734004d0596fc1ff1f73d
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2023-04-18 20:16:50 -0300

    feat: model

--- projeto_integrador/registro_de_animal/models.py
+++ projeto_integrador/registro_de_animal/models.py
@@ -1,27 +1,32 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-# Create your models here.
-
-class TaskItem(models.Model):
-    DEFAULT = 'DEFAULT'
-    PERSONAL  =  'PERSONAL'
-    SHOPPING = 'SHOPPING'
-    WISHLIST = 'WISHLIST'
-    WORK =' WORK'
-    CATEGORIES = (
-        (DEFAULT,DEFAULT),
-        (PERSONAL,PERSONAL),
-        (SHOPPING,SHOPPING),
-        (WISHLIST,WISHLIST),
-        (WORK,WORK)
-    )
-    title = models.CharField(max_length=100,blank=True,null=True)
-    body =models.TextField(null= True,blank=True)
-    due_date = models.DateField(default= timezone.now)
-    task_finished = models.BooleanField(default= True)
-    category = models. CharField(max_length= 20, choices= CATEGORIES , default= DEFAULT)
+class Author(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
 
     def __str__(self):
-        return f'{self.title}'
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
