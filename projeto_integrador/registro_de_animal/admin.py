e2b917d4a25c3aeb333e1930aa4d5a319a612bec
gfontes1 <gfontes789@gmail.com>
2023-04-22 16:00:14 -0300

    models | added 'Vaccines'

--- projeto_integrador/registro_de_animal/admin.py
+++ projeto_integrador/registro_de_animal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import *
+from .models.animals import *
+from .models.vaccines import *
 
 admin.site.register(Tutor)
 admin.site.register(Especie)
@@ -7,3 +8,5 @@ admin.site.register(Sexo)
 admin.site.register(Porte)
 admin.site.register(Raça)
 admin.site.register(Animal)
+
+admin.site.register(Vacina)
